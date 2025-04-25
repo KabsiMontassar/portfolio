4ac072f5d3ffca3a3c0519cdf0e339ae4c8a8a79
Kebsi Montassar <114872623+KabsiMontassar@users.noreply.github.com>
2025-04-25 20:29:33 +0100

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,33 +3,33 @@ import { Link as RouterLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <Box  pt={14} width="100%" px={4}>
+    <Box pt={14} width="100%" px={4}>
       <Flex
         maxW="630px"
         mx="auto"
         py={4}
         px={8}
         bg="white"
-       
+        position="relative"
         borderRadius="full"
-        boxShadow="sm"
+        boxShadow="md"
         justify="center"
         align="center"
         gap={8}
       >
-        <ChakraLink as={RouterLink} to="/" fontSize="xl" fontWeight="700" _hover={{  textDecoration: "underline" }}>
+        <ChakraLink as={RouterLink} to="/" fontSize="xl" fontWeight="550" position="relative" _hover={{ textDecoration: "underline" }}>
           Kebsi Montassar
         </ChakraLink>
-        <ChakraLink as={RouterLink} to="/about" fontSize="md" fontWeight="500" _hover={{  fontWeight: "700" }}>
+        <ChakraLink as={RouterLink} to="/about" fontSize="md" fontWeight="500" position="relative" _hover={{ fontWeight: "700" }}>
           About
         </ChakraLink>
-        <ChakraLink as={RouterLink} to="/experience" fontSize="md" fontWeight="500" _hover={{fontWeight: "700" }}>
+        <ChakraLink as={RouterLink} to="/experience" fontSize="md" fontWeight="500" position="relative" _hover={{ fontWeight: "700" }}>
           Experiences
         </ChakraLink>
-        <ChakraLink as={RouterLink} to="/projects" fontSize="md" fontWeight="500" _hover={{  fontWeight: "700" }}>
+        <ChakraLink as={RouterLink} to="/projects" fontSize="md" fontWeight="500" position="relative" _hover={{ fontWeight: "700" }}>
           Projects
         </ChakraLink>
-        <ChakraLink as={RouterLink} to="/contact" fontSize="md" fontWeight="500" _hover={{  fontWeight: "700" }}>
+        <ChakraLink as={RouterLink} to="/contact" fontSize="md" fontWeight="500" position="relative" _hover={{ fontWeight: "700" }}>
           Contact
         </ChakraLink>
       </Flex>
