a55113847b0a5c841741e29e9ce781dfc3aca526
Kebsi Montassar <114872623+KabsiMontassar@users.noreply.github.com>
2025-04-25 20:58:39 +0100

    Update page.jsx

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -6,33 +6,25 @@ export default function Page() {
     const [isActive, setIsActive] = useState(false);
 
     return (
-        <Box 
+        <Box
             position="absolute"
             top={0}
             left={0}
             right={0}
             bottom={0}
-            display="flex" 
-            alignItems="center" 
-            justifyContent="center" 
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
             zIndex={10}
         >
             <VStack spacing={6} align="center">
-                <Heading
+                <Text
                     onMouseEnter={() => setIsActive(true)}
                     onMouseLeave={() => setIsActive(false)}
-                    fontSize={["3xl", "4xl", "5xl"]}
-                    textAlign="center"
-                    maxW="800px"
-                    p={8}
-                >
-                    Hi, I'm Kebsi Montassar
-                </Heading>
-                <Text
-                    fontSize={["lg", "xl"]}
+                    fontSize={"7xl"}
                     color="gray.600"
                     fontStyle="italic"
-                    maxW="600px"
+                    maxW="1000px"
                     textAlign="center"
                 >
                     "The only way to do great work is to love what you do"
