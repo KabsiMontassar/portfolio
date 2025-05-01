74f86832f71edd7a8a0ff6612051575f55367074
Marwa <114872623+KabsiMontassar@users.noreply.github.com>
2025-05-01 18:49:18 +0100

    Update experience.jsx

--- src/pages/experience.jsx
+++ src/pages/experience.jsx
@@ -1,8 +1,121 @@
 import React from 'react'
+/**
+ Mood and Food
+Full Stack Developer Intern
+Ariana, Tunis
+August 2024–January 2025
+•
+Designed and developed a full-stack e-commerce web application for diet products, gym accessories, and nutritional documentation, featuring AI-based product recommendations using TensorFlow and OpenAI.
+•
+Created modern, responsive UI/UX designs using Figma and implemented them with ReactJS and Chakra UI.
+•
+Built the back-end using Node.js and Express, with RESTful APIs for secure data handling and JWT for authentication.
+•
+Implemented real-time data management using Firebase Firestore and Firebase Extensions.
+•
+Conducted API testing and debugging using Postman.
+Technologies: Full Stack Development, E-commerce, RESTful APIs, Firebase, Firebase Extensions, Security Best Practices, AI (OpenAI), Recommendations, HTML, CSS, JavaScript, ReactJS, Cross-Browser Compatibility, Chakra UI, Figma, Postman.
+*/
+const data = [
+  {
+    title: 'Full Stack Web Developer Intern',
+    company: 'FIDNESS',
+    location: 'La Marsa, Tunisia',
+    startDate: '2023-2-01',
+    endDate: '2023-6-31',
+    Description : [
+      'Designed and developed a web application for managing partner contracts, implementing encryption and other security measures to protect sensitive data.',
+      'Designed and implemented data visualization features to improve data presentation, enabling efficient management and enhancing client retention.'
+    ],
+    Technologies: [
+       'HTML',
+      'CSS',
+      'JavaScript',
+      'ReactJS',
+      'NodeJS',
+      'RESTful APIs',
+      'Full Stack Development',
+      'Cross-Browser Compatibility',
+      'Security Best Practices',
+      'Encryption',
+      'Data Visualization',
+      'ChartJs',
+      'MongoDB',
+      'Mongoose',
+      'Postman',
+      'Git'
+    ],
+    linkToCompany: 'https://www.linkedin.com/company/fidness-company/',
+   
+  },
+  {
+    title: 'Full Stack Web Developer Intern',
+    company: 'ESPRIT',
+    location: 'Ariana, Tunisia',
+    startDate: '2024-6-01',
+    endDate: '2024-7-31',
+    Description : [
+      'Developed an e-commerce platform for plant sales with category filtering and Algolia search.',
+      'Implemented stateless authentication using JWT.',
+      'Built AI models for plant disease diagnosis and plant recognition with care recommendations.',
+      'Designed a user-friendly interface with Chakra UI and Figma.',
+      'Utilized Firebase for backend services and tested APIs with Postman.'
+    ],
+    Technologies: [
+      'Full Stack Development',
+      'E-commerce',
+      'AI',
+      'JWT Authentication',
+      'Firebase',
+      'Algolia API',
+      'Chakra UI',
+      'Figma',
+      'Cross-Browser Compatibility',
+      'ReactJS',
+      'Postman',
+      'Security Best Practices'
+    ],
+    linkToCompany: `https://www.linkedin.com/school/ecole-supérieure-privée-d'ingénierie-et-de-technologies---esprit/`
+ },
+  {
+    title: 'Full Stack Web Developer Intern',
+    company: 'Mood and Food',
+    location: 'Ariana, Tunisia',
+    startDate: '2024-8-01',
+    endDate: '2025-1-31',
+    Description : [
+      'Designed and developed a full-stack e-commerce web application for diet products, gym accessories, and nutritional documentation, featuring AI-based product recommendations using TensorFlow and OpenAI.',
+      'Created modern, responsive UI/UX designs using Figma and implemented them with ReactJS and Chakra UI.',
+      'Built the back-end using Node.js and Express, with RESTful APIs for secure data handling and JWT for authentication.',
+      'Implemented real-time data management using Firebase Firestore and Firebase Extensions.',
+      'Conducted API testing and debugging using Postman.'
+    ],
+    Technologies: [
+      'Full Stack Development',
+      'E-commerce',
+      'RESTful APIs',
+      'Firebase',
+      'Firebase Extensions',
+      'Security Best Practices',
+      'AI (OpenAI)',
+      'Recommendations',
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'ReactJS',
+      'Cross-Browser Compatibility',
+      'Chakra UI',
+      'Figma',
+      'Postman'
+    ],
+    linkToCompany: `https://www.linkedin.com/company/mood-and-food/`
+  }
+
+]
 
 const experience = () => {
   return (
-    <div>experience</div>
+    <div>hello</div>
   )
 }
 
