e0abcff475184f71f3831e5c293838e8cbe7fb72
Kebsi Montassar <114872623+KabsiMontassar@users.noreply.github.com>
2025-05-11 01:23:42 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/',
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', '@chakra-ui/react', 'framer-motion'],
+          i18n: ['i18next', 'react-i18next'],
+        }
+      }
+    },
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
